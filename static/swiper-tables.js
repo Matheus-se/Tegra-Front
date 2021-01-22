3ef2f8bfe94d8cf0d6c94abe5d11c456ebfd2821
Matheus-se <63105495+Matheus-se@users.noreply.github.com>
2021-01-22 16:27:17 -0300

    Add files via upload

--- static/swiper-tables.js
+++ static/swiper-tables.js
@@ -1,21 +1,21 @@
 import Swiper from "https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js";
 
-const swiper = new Swiper(".swiper-container", {
-  slidesPerView: 3,
-  spaceBetween: 0,
+// const swiper = new Swiper(".swiper-container", {
+//   slidesPerView: 3,
+//   spaceBetween: 0,
 
-  breakpoints: {
-    480: {
-      slidesPerView: 5,
-    },
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+//   breakpoints: {
+//     480: {
+//       slidesPerView: 5,
+//     },
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
 
-const swiper2 = new Swiper(".swiper-container-home", {
+const swiper = new Swiper(".swiper-container-home", {
   slidesPerView: 1,
   effect: 'fade',
   fadeEffect: {
