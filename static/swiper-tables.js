4e48538eb10418ec0dcb3e4693f8508b261391b7
Matheus-se <63105495+Matheus-se@users.noreply.github.com>
2021-01-25 10:17:55 -0300

    Add files via upload

--- static/swiper-tables.js
+++ static/swiper-tables.js
@@ -1,34 +1,21 @@
 import Swiper from "https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js";
 
-// const swiper = new Swiper(".swiper-container", {
-//   slidesPerView: 3,
-//   spaceBetween: 0,
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 0,
 
-//   breakpoints: {
-//     480: {
-//       slidesPerView: 5,
-//     },
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+  breakpoints: {
+    480: {
+      slidesPerView: 5,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-// const swiper2 = new Swiper(".swiper-container-home", {
-//   slidesPerView: 1,
-//   effect: 'fade',
-//   fadeEffect: {
-//     crossFade: true
-//   },
-  
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
-
-const swiper3 = new Swiper(".swiper-container-quarters", {
+const swiper2 = new Swiper(".swiper-container-home", {
   slidesPerView: 1,
   effect: 'fade',
   fadeEffect: {
@@ -38,5 +25,13 @@ const swiper3 = new Swiper(".swiper-container-quarters", {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper3 = new Swiper(".swiper-container-quarters", {
+  slidesPerView: "auto",
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
   },
 });
